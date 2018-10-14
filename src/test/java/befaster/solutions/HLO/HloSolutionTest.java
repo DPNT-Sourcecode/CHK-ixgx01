f57116ef14f0a19d6df1985b49b549a912c6ac09
sbx_user1060 <sbx_user1060@ip-10-129-68-6.eu-west-2.compute.internal>
2018-10-14 19:18:23 +0000

    Sun Oct 14 19:18:23 UTC 2018

--- src/test/java/befaster/solutions/HLO/HloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HloSolutionTest.java
@@ -1,14 +1,24 @@
 package befaster.solutions.HLO;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import befaster.solutions.TST.One;
+
 public class HloSolutionTest {
+    private HelloSolution hello;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void name() {
+        hello = new HelloSolution();
+    }
 
+    @Test
+    public void run() {
+        assertThat(hello.hello("Fred Smith"), equalTo("Fred Smith"));
+    }
 }
