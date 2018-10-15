f6bb6fb49bd4586604617bc501d597e1c5934818
sbx_user1060 <sbx_user1060@ip-10-130-104-56.eu-west-2.compute.internal>
2018-10-15 21:32:32 +0000

    Mon Oct 15 21:32:32 UTC 2018

--- src/test/java/befaster/solutions/CHK/ChkSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/ChkSolutionTest.java
@@ -6,17 +6,17 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class SumSolutionTest {
-    private SumSolution sum;
+public class ChkSolutionTest {
+    private CheckoutSolution chk;
 
     @Before
     public void setUp() {
 
-        sum = new SumSolution();
+        chk = new CheckoutSolution();
     }
 
     @Test
     public void compute_sum() {
-        assertThat(sum.compute(1, 1), equalTo(2));
+        assertThat(chk.checkout("ABCD"), equalTo(115));
     }
 }
