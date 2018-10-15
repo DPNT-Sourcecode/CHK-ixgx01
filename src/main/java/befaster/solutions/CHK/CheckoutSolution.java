b80916f0a9a1df74520978b020f10d4a819f1449
sbx_user1060 <sbx_user1060@ip-10-130-104-56.eu-west-2.compute.internal>
2018-10-15 21:47:32 +0000

    Mon Oct 15 21:47:32 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,8 +3,15 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-	static final int [] specials = {{50,3,130},{30,2,45},{20},{15}};
+	static final int [][] specials = {{50,3,130},{30,2,45},{20},{15}};
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    	if (skus == "" || skus == null) return -1;
+        //Parse string checking for invalid characters
+    	for (int i=0;i < skus.length(); i++) {
+    		if (skus.charAt(i) < 'A' || skus.charAt(i) > ('A' + specials.length - 1)) {
+    			return -1;
+    		}
+    	}
+    	return 0;
     }
 }
