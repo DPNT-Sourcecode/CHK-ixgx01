10b2db303a4c19705e6fcee1edef338e55ee4c0c
sbx_user1060 <sbx_user1060@ip-10-130-104-56.eu-west-2.compute.internal>
2018-10-15 21:29:32 +0000

    Mon Oct 15 21:29:32 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,6 +3,7 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+	static final int [] specials = {{50,3,130},{30,2,45},{20},{15}};
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
     }
